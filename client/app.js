bae793466fa3fb1a6a3518c87b1ffe5fbcaffd3f
Max Poon <maxpanziyuan@gmail.com>
2019-01-26 21:29:27 +0800

    add comment

--- client/app.js
+++ client/app.js
@@ -1,13 +1,11 @@
 //app.js
-var qcloud = require('./vendor/wafer2-client-sdk/index')
-var config = require('./config')
-
 App({
   onLaunch: function() {
     var that = this;
+
+    // Login and get the user information
     wx.login({
       success(res) {
-        var code = res.code;
         wx.getUserInfo({
           withCredentials: true,
           success: function(res) {
