aa8f1222b93737f2e48642cda5e49b1fb1f68663
Max Poon <maxpanziyuan@gmail.com>
2019-01-29 00:34:51 +0800

    add user url

--- client/config.js
+++ client/config.js
@@ -7,22 +7,22 @@ var host = 'https://aeoo5f7q.qcloud.la';
 
 var config = {
 
-    // 下面的地址配合云端 Demo 工作
-    service: {
-        host,
+  // 下面的地址配合云端 Demo 工作
+  service: {
+    host,
 
-        // 登录地址，用于建立会话
-        loginUrl: `${host}/weapp/login`,
+    // 登录地址，用于建立会话
+    loginUrl: `${host}/weapp/login`,
 
-        // 测试的请求地址，用于测试会话
-        requestUrl: `${host}/weapp/user`,
+    // User api url
+    userUrl: `${host}/weapp/user`,
 
-        // 测试的信道服务地址
-        tunnelUrl: `${host}/weapp/tunnel`,
+    // 测试的信道服务地址
+    tunnelUrl: `${host}/weapp/tunnel`,
 
-        // 上传图片接口
-        uploadUrl: `${host}/weapp/upload`
-    }
+    // 上传图片接口
+    uploadUrl: `${host}/weapp/upload`
+  }
 };
 
 module.exports = config;
