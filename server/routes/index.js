52f46c81a789e8434426bbe01686c5bfe37c10fc
Max Poon <maxpanziyuan@gmail.com>
2019-01-29 00:25:51 +0800

    add user api endpoints

--- server/routes/index.js
+++ server/routes/index.js
@@ -13,8 +13,10 @@ const { auth: { authorizationMiddleware, validationMiddleware } } = require('../
 // --- 登录与授权 Demo --- //
 // 登录接口
 router.get('/login', authorizationMiddleware, controllers.login)
-// 用户信息接口（可以用来验证登录态）
-router.get('/user', validationMiddleware, controllers.user)
+
+// User API endpoints
+router.get('/user', validationMiddleware, controllers.user.get)
+router.put('/user', validationMiddleware, controllers.user.put)
 
 // --- 图片上传 Demo --- //
 // 图片上传接口，小程序端可以直接将 url 填入 wx.uploadFile 中
