41989c32a5d5040aea880bd0146923a6374502bc
ZhangYihanNus <zhangyh0002@gmail.com>
2019-01-30 23:22:51 +0800

    Version 1.1

--- client/pages/me/me.js
+++ client/pages/me/me.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //index.js
 //获取应用实例
 const app = getApp()
@@ -6,7 +5,6 @@ const app = getApp()
 Page({
   data: {
     userInfo: {},
-    hasUserInfo: false,
     chName: null,
     chNameTemp: null,
     egName: null,
@@ -23,9 +21,9 @@ Page({
 
   onLoad: function () {
     
-    if (this.data.hasUserInfo == false) {
+    if (this.data.userInfo == null) {
         // if there is no userinfo yet
-        setTimeout(function () {}, 100)
+        //setTimeout(function () {}, 100)
     } 
   },
  
@@ -69,71 +67,3 @@ Page({
   }
 
 })
-=======
-// pages/me/me.js
-Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
-})
->>>>>>> TPCC/master
