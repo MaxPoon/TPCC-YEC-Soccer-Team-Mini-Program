915bfab7e25bc8c12d7633b332d843b4a377473a
Max Poon <maxpanziyuan@gmail.com>
2019-02-03 23:04:01 +0800

    create server test mode

--- server/tools/cAuth.sql
+++ server/tools/cAuth.sql
@@ -21,6 +21,7 @@ SET FOREIGN_KEY_CHECKS = 0;
 --  Table structure for `cSessionInfo`
 -- ----------------------------
 DROP TABLE IF EXISTS `cSessionInfo`;
+DROP TABLE IF EXISTS `User`;
 CREATE TABLE `cSessionInfo` (
   `open_id` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `uuid` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -33,5 +34,33 @@ CREATE TABLE `cSessionInfo` (
   KEY `openid` (`open_id`) USING BTREE,
   KEY `skey` (`skey`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='会话管理用户信息';
+
+--
+-- Table structure for table `User`
+--
+
+CREATE TABLE `User` (
+  `OpenId` varchar(255) NOT NULL,
+  `WechatName` varchar(255) NOT NULL,
+  `Birthday` date NOT NULL,
+  `ChineseName` varchar(255) DEFAULT NULL,
+  `EnglishName` varchar(255) DEFAULT NULL,
+  `EmailAddress` varchar(255) NOT NULL,
+  `MobileNumber` varchar(255) NOT NULL,
+  `IsAdmin` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `User`
+--
+ALTER TABLE `User`
+  ADD PRIMARY KEY (`OpenId`),
+  ADD KEY `Name_Index` (`WechatName`),
+  ADD KEY `Birthday_Index` (`Birthday`);
+COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
