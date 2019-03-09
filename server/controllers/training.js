063f78b17b0b0158c5eccea3ddde423113b842b5
Eric Ma <eric.m.990909@gmail.com>
2019-03-09 16:22:29 +0800

    Update get method for training controller

--- server/controllers/training.js
+++ server/controllers/training.js
@@ -3,8 +3,21 @@ const { mysql } = require('../qcloud.js');
 const trainingTableName = "Training";
 
 async function get(ctx) {
-  console.log(mysql(trainingTableName).max('UpdatedTime'));
-  ctx.body = mysql(trainingTableName).max('UpdatedTime');
+  
+  // var training = await mysql(trainingTableName).where({ UpdatedTime: "2019-01-01 00:00:00"});
+
+  var training_time = await mysql(trainingTableName).max("UpdatedTime").first();
+  // console.log("*************************** RESPONSE *********************************")
+  // console.log(training_time);
+  training_time = Object.values(training_time)[0];
+  // console.log(training_time);
+  var mydate = new Date(training_time);
+  // console.log(mydate);
+  var result = mydate.toString('dddd MMM yyyy hh:mm:ss');
+  // console.log("*************************** RESPONSE *********************************")
+  // console.log(result);
+  var training = await mysql(trainingTableName).where({ UpdatedTime: training_time });
+  ctx.body = training;
 }
 
 
