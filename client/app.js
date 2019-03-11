054f0b74a9257102685f87f69dc0076a6b6f7620
Max Poon <maxpanziyuan@gmail.com>
2019-03-12 00:38:09 +0800

    add login page

--- client/app.js
+++ client/app.js
@@ -6,33 +6,5 @@ App({
   onLaunch: function() {
     var that = this;
     qcloud.setLoginUrl(config.service.loginUrl);
-
-    // Login and store the user information
-    qcloud.login({
-      success(res) {
-        that.userInfo = res;
-        qcloud.request({
-          url: config.service.userUrl + '?OpenId=' + that.userInfo.openId,
-          method: 'GET', 
-          success: function (res) {
-            //able to get user's personal information from server
-            that.userInfo = Object.assign(that.userInfo, res.body);
-          },
-          fail: function (err) {
-            // fail
-            console.log('failed to get userinfo; err:');
-            console.log(err);
-          },
-          complete: function () {
-            // complete
-            console.log('complete qcloud.request for getting userinfo');
-          }
-        });
-      },
-      fail(err) {
-        console.log('fail to login; err:');
-        console.log(err);
-      }
-    });
   }
 });
