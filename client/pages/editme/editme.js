8286b3a6987cf91a7a2dbb3f8080643d7d96a7f9
ZhangYihanNus <zhangyh0002@gmail.com>
2019-03-18 00:45:08 +0800

    Changed me page data editing

--- client/pages/editme/editme.js
+++ client/pages/editme/editme.js
@@ -1,1 +1,100 @@
 //pages/editme/editme.js
+const qcloud = require('wafer2-client-sdk');
+const app = getApp();
+const config = request('../../congig');
+
+Page({
+
+  data: {
+    personal: null,
+    chineseName: null,
+    englishName: null,
+    phoneNumber: null,
+    email: null,
+    birthday: null,
+    btnDisable: true
+  },
+
+  onLoad: function(options) {
+    this.setData({
+      personal: app.userInfo
+    })
+  },
+
+  formSubmit: function (e) { },
+
+  bindBirthdayChange(e) {
+    this.setData({
+      birthday: e.detail.value,
+      btnDisable: 0
+    })
+  },
+  chineseNameInput: function (event) {
+    this.setData({
+      chineseName: event.detail.value,
+      btnDisable: event.detail.value.length === 0
+    });
+  },
+  englishNameInput: function (event) {
+    this.setData({
+      englishName: event.detail.value,
+      btnDisable: event.detail.value.length === 0
+    });
+  },
+  phoneInput: function (event) {
+    this.setData({
+      phoneNumber: event.detail.value,
+      btnDisable: event.detail.value.length === 0
+    });
+  },
+  emailInput: function (event) {
+    this.setData({
+      email: event.detail.value,
+      btnDisable: event.detail.value.length === 0
+    });
+  },
+  formSubmit(e) {
+    //update the latest info into server
+    qcloud.request({
+      // url: 'http://localhost:5757',
+      url: config.service.userUrl,
+      method: 'PUT',
+      // header: {
+      //   'content-type':'application/x-www-form-urlencoded'
+      // },
+      data: {
+        OpenId: this.data.personal.openId,
+        WechatName: this.data.personal.nickName,
+        ChineseName: this.data.chineseName,
+        EnglishName: this.data.englishName,
+        MobileNumber: this.data.phone,
+        EmailAddress: this.data.email,
+        Birthday: this.data.birthday
+      },
+      success: function (res) {
+        //able to update userinfo into server
+      },
+      fail: function (err) {
+        // fail
+        console.log('failed to update userinfo; err:');
+        console.log(err);
+      },
+      complete: function () {
+        // complete
+      }
+    });
+    this.setData({
+      btnDisable: true
+    });
+
+    var pages = getCurrentPages();
+    var prevPage = pages[pages.length - 2];
+    prevPage.setData({
+      chineseName: this.data.chineseName,
+      englishName: this.data.englishName,
+      phoneNumber: this.data.phoneNumber,
+      email: this.data.email,
+      birthday: this.data.birthday
+    });
+  }
+})
--- client/pages/me/me.js
+++ client/pages/me/me.js
@@ -1,16 +1,15 @@
 // pages/me/me.js
 const qcloud = require('wafer2-client-sdk');
 const app = getApp();
-//const config = request('../../congig');
 
 Page({
   data: {
     personal: {},
-    chineseNameInputValue: null,
-    englishNameInputValue: null,
-    phoneInputValue: null,
-    emailInputValue: null,
-    btnDisable: true
+    chineseName: null,
+    englishName: null,
+    phoneNumber: null,
+    email: null,
+    birthday: null
   },
   
   //jump to editme page
@@ -27,64 +26,4 @@ Page({
     console.log(app.userInfo);
   },
 
-  chineseNameInput: function (event) {
-    this.setData({
-      chineseNameInputValue: event.detail.value,
-      btnDisable: event.detail.value.length === 0
-    });
-  },
-  englishNameInput: function (event) {
-    this.setData({
-      englishNameInputValue: event.detail.value,
-      btnDisable: event.detail.value.length === 0
-    });
-  },
-  phoneInput: function (event) {
-    this.setData({
-      phoneInputValue: event.detail.value,
-      btnDisable: event.detail.value.length === 0
-    });
-  },
-  emailInput: function (event) {
-    this.setData({
-      emailInputValue: event.detail.value,
-      btnDisable: event.detail.value.length === 0
-    });
-  },
-
-
-  infoSaveClick: function (event) {
-    //update the latest info into server
-    qcloud.request({
-      url: 'http://localhost:5757',
-      //url: config.service.userUrl,
-      method: 'PUT',
-      // header: {
-      //   'content-type':'application/x-www-form-urlencoded'
-      // },
-      data: {
-        OpenId: this.data.personal.openId,
-        WechatName: this.data.personal.nickName,
-        ChineseName: this.data.chineseNameInputValue,
-        EnglishName: this.data.englishNameInputValue,
-        MobileNumber: this.data.phoneInputValue,
-        EmailAddress: this.data.emailInputValue,
-        Birthday: '1999-09-19'
-      },
-      success: function (res) {
-        //able update userinfo into server
-      },
-      fail: function (err) {
-        // fail
-        console.log('failed to update userinfo; err:');
-        console.log(err);
-      },
-      complete: function () {
-        // complete
-      }
-    });
-    this.setData({
-      btnDisable: true
-    });
-  }
 });
